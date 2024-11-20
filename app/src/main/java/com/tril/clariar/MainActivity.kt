30b698658259426f3964ff3ccd2b4218472140e0
Miguel Queiroz <miguel.queiroz.fernandes@gmail.com>
2024-11-20 17:47:59 -0300

    Updating Permission screen: adding logo

--- app/src/main/java/com/tril/clariar/MainActivity.kt
+++ app/src/main/java/com/tril/clariar/MainActivity.kt
@@ -6,6 +6,7 @@ import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
@@ -13,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.tril.clariar.ui.theme.ClarIArTheme
@@ -44,7 +46,6 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun PermissionsScreen(onNavigateToSettings: () -> Unit, modifier: Modifier = Modifier) {
-    // Tela com botão centralizado
     Box(
         contentAlignment = Alignment.Center,
         modifier = modifier.fillMaxSize()
@@ -53,8 +54,21 @@ fun PermissionsScreen(onNavigateToSettings: () -> Unit, modifier: Modifier = Mod
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
+            // Exibe a logo do app
+            Image(
+                painter = painterResource(id = R.drawable.app_logo),
+                contentDescription = "ClarIAr logo",
+                modifier = Modifier
+                    .width(200.dp) // Define a largura
+                    .height(100.dp) // Define a altura
+                    .padding(bottom = 12.dp) // Espaçamento abaixo da logo
+            )
+
+            // Texto explicativo
             Text(text = "O app precisa de permissão para captura de tela.")
             Spacer(modifier = Modifier.height(16.dp))
+
+            // Botão para abrir as configurações
             Button(onClick = onNavigateToSettings) {
                 Text(text = "Abrir Configurações")
             }
@@ -69,4 +83,3 @@ fun PermissionsScreenPreview() {
         PermissionsScreen(onNavigateToSettings = {})
     }
 }
- 
